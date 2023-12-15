51d85efc15ca86d2ae340f7335066d1b5ca8eabb
Krazick <markjsmith60@gmail.com>
2023-12-15 15:07:36 -0500

    Copied over from 18XX-Game-Engine

---
+++ Game Engine Utilities/src/geUtilities/xml/LoadableXMLI.java
@@ -0,0 +1,22 @@
+package geUtilities.xml;
+
+import java.io.IOException;
+
+//
+//  LoadableXMLI.java
+//  Game_18XX
+//
+//  Created by Mark Smith on 11/28/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+import geUtilities.ParsingRoutineI;
+import geUtilities.XMLDocument;
+
+public interface LoadableXMLI extends ParsingRoutineI {
+	/* Returns the name of the Loadable Object for Error Messages */
+	public String getTypeName ();
+
+	/* Loads from a XML Document the object, throws one IOException */
+	public void loadXML (XMLDocument aXMLDocument) throws IOException;
+}
---
+++ Game Engine Utilities/src/geUtilities/xml/NetworkGameSupport.java
@@ -0,0 +1,47 @@
+package geUtilities.xml;
+
+import java.awt.Point;
+
+import org.apache.logging.log4j.Logger;
+
+import geUtilities.FileUtils;
+import geUtilities.XMLElement;
+
+public interface NetworkGameSupport {
+	public final String NO_NAME = "";
+	public static final String NO_GAME_ID = "NOID";
+	public static final String EMPTY_GAME_ID = "";
+	public final static int WAIT_TIME = 10;
+
+	public void updatePlayerCountLabel ();
+	public void addNetworkPlayer (String aPlayerName);
+	public void removeNetworkPlayer (String aPlayerName);
+	public void removeAllNetworkPlayers ();
+	public void handleGameActivity (String aGameActivity);
+	public String getPlayersInOrder ();
+	public void randomizePlayerOrder ();
+	public void initiateNetworkGame ();
+	public boolean gameStarted ();
+	public Point getOffsetGEFrame ();
+	public String getClientUserName ();
+	public void parseNetworkSavedGames (String aResponse);
+	public void loadAutoSavedGame (String aAutoSaveFileName);
+	public void updateDisconnectButton ();
+	public FileUtils getFileUtils ();
+	
+	public int getSelectedGameIndex ();
+	public void setSelectedGameIndex (int aGameIndex);
+
+	public void addNewFrame (XMLFrame aJGameClient);
+
+	public Logger getLogger ();
+	public String getXMLBaseDirectory ();
+	public XMLElement getGameVariantsXMLElement ();
+	
+	public void setGameID (String aGameID);
+	public String getGameID ();
+	public void resetGameID (String aGameID);
+
+	public String getGEVersion ();
+	public String getEnvironmentVersionInfo ();
+}
