cd5c01c06149b73e4d9254371dadc6b5edc90bf4
Krazick <markjsmith60@gmail.com>
2024-08-08 13:39:34 -0400

    New utility to generate MD5 Sum from XMLDocument or String

---
+++ Game Engine Utilities/src/geUtilities/Checksum.java
@@ -0,0 +1,32 @@
+package geUtilities;
+
+public class Checksum {
+
+	public Checksum () {
+		
+	}
+	
+	public String MD5 (XMLDocument aXMLDocument) {
+		return MD5 (aXMLDocument.toString ());
+	}
+	
+	public String MD5 (String aMessage) {
+	   try {
+	        java.security.MessageDigest tMD = java.security.MessageDigest.getInstance ("MD5");
+	        byte [] tBytes = tMD.digest (aMessage.getBytes ());
+	        StringBuffer tStringBuffer = new StringBuffer ();
+	        
+	        for (int i = 0; i < tBytes.length; ++i) {
+	          tStringBuffer.append (Integer.toHexString ((tBytes [i] & 0xFF) | 0x100).substring (1, 3));
+	        }
+	        
+	        return tStringBuffer.toString ();
+	    } catch (java.lang.NullPointerException eNull) {
+	    	
+	    } catch (java.security.NoSuchAlgorithmException eNSA) {
+	    	
+	    }
+	   
+	    return null;
+	}
+}
