3d95184bcfd00c4d768fcedaa7b45ab947262f4f
Krazick <markjsmith60@gmail.com>
2024-08-10 16:26:10 -0400

    Functionality to find and remove a Checksum (support for UNDO)

--- Game Engine Utilities/JUnitSrc/geUtilities/utilites/Checksums.java
+++ Game Engine Utilities/JUnitSrc/geUtilities/utilites/Checksums.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Checksums {
 	public static final Checksums NO_CHECKSUMS = null;
+	public static final int NOT_FOUND = -1;
 	List<Checksum> checksums;
 
 	public Checksums () {
@@ -15,11 +16,53 @@ public class Checksums {
 		checksums.add (aChecksum);
 	}
 	
-	public Checksum getAt (int aIndex) {
+	public Checksum get (int aIndex) {
 		return checksums.get (aIndex);
+	}
+	
+	public int findIndexFor (int aActionNumber) {
+		int tIndex;
+		int tCount;
+		int tFoundIndex;
+		Checksum tChecksum;
+		
+		tFoundIndex = NOT_FOUND;
+		if (!checksums.isEmpty ()) {
+			tCount = size ();
+			for (tIndex = 0; tIndex < tCount; tIndex++) {
+				tChecksum = checksums.get (tFoundIndex);
+				if (tChecksum.getActionNumber () == aActionNumber) {
+					tFoundIndex = tIndex;
+				}
+			}
+			
+		}
+		
+		return tFoundIndex;
+	}
+	
+	public void removeAtNumber (int aActionNumber) {
+		int tIndex;
+		
+		tIndex = findIndexFor (aActionNumber);
+		if (tIndex != NOT_FOUND) {
+			remove (tIndex);
+		}
+	}
+	
+	public void remove (int aIndex) {
+		checksums.remove (aIndex);
 	}
 	
 	public int size () {
 		return checksums.size ();
+	}
+	
+	public void printChecksums () {
+		if (!checksums.isEmpty ()) {
+			for (Checksum tChecksum : checksums) {
+				tChecksum.printInfo ();
+			}
+		}
 	}
 }
