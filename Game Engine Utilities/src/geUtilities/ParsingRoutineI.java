c1f2fc526e53307165753162959891511e1ac6cf
Krazick <markjsmith60@gmail.com>
2024-09-03 15:15:07 -0400

    New method 'getObject' to return the object that was parsed out

--- Game Engine Utilities/src/geUtilities/ParsingRoutineI.java
+++ Game Engine Utilities/src/geUtilities/ParsingRoutineI.java
@@ -3,6 +3,10 @@ package geUtilities;
 import geUtilities.xml.XMLNode;
 
 public interface ParsingRoutineI {
+	public default Object getObject () {
+		return "Default Parsing Routine Object";
+	}
+
 	/* Support Callback Functions when a XML Node Name matches a specified value */
 	public default void foundItemMatchKey1 (XMLNode aChildNode) {
 
