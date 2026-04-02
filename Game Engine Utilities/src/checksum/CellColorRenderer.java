da19af1d49318c4c98ec30769b61d72f52f2419f
Krazick <markjsmith60@gmail.com>
2026-04-02 15:47:54 -0400

    Formatting

--- Game Engine Utilities/src/checksum/CellColorRenderer.java
+++ Game Engine Utilities/src/checksum/CellColorRenderer.java
@@ -31,43 +31,44 @@ public class CellColorRenderer extends JLabel implements TableCellRenderer {
 	
     @Override
     public Component getTableCellRendererComponent (JTable aTable, Object aValue,
-            boolean aIsSelected, boolean aHasFocus, int aRow, int aColumn) {
-    		int tFirstChecksumIndex;
-    		int tLastChecksumIndex;
-    		int tColumnIndex;
-    		String tChecksumValue;
-    		String tFirstChecksumValue;
-    		Component tCellRenderer;
-    		TableModel tTableModel;
-    		boolean tIdenticalChecksums;
-    		
+            			boolean aIsSelected, boolean aHasFocus, int aRow, int aColumn) {
+		int tFirstChecksumIndex;
+		int tLastChecksumIndex;
+		int tColumnIndex;
+		String tChecksumValue;
+		String tFirstChecksumValue;
+		Component tCellRenderer;
+		TableModel tTableModel;
+		boolean tIdenticalChecksums;
+		
        	tIdenticalChecksums = true;
        	tFirstChecksumValue = null;
 		tFirstChecksumIndex = firstChecksumColumn;
-		tCellRenderer = DEFAULT_RENDERER.getTableCellRendererComponent (aTable, aValue, aIsSelected, aHasFocus, 
-							aRow, aColumn);
+		tCellRenderer = DEFAULT_RENDERER.getTableCellRendererComponent (aTable, aValue, aIsSelected, 
+								aHasFocus, aRow, aColumn);
   		if (aColumn >= tFirstChecksumIndex) {
-	    		tTableModel = aTable.getModel ();
-	    		tLastChecksumIndex = tTableModel.getColumnCount () - 1;
-	    		while ((tFirstChecksumValue == null) && (tFirstChecksumIndex < tLastChecksumIndex)) {
-	    			tFirstChecksumValue = (String) tTableModel.getValueAt (aRow, tFirstChecksumIndex++);
-	    		}
-	
-	    		if (tFirstChecksumValue != null) {
-	    			tFirstChecksumIndex = firstChecksumColumn;
+    		tTableModel = aTable.getModel ();
+    		tLastChecksumIndex = tTableModel.getColumnCount () - 1;
+    		while ((tFirstChecksumValue == null) && (tFirstChecksumIndex < tLastChecksumIndex)) {
+    			tFirstChecksumValue = (String) tTableModel.getValueAt (aRow, tFirstChecksumIndex++);
+    		}
+
+    		if (tFirstChecksumValue != null) {
+    			tFirstChecksumIndex = firstChecksumColumn;
 		     	for (tColumnIndex = tFirstChecksumIndex; tColumnIndex <= tLastChecksumIndex; tColumnIndex++) {
 		     		tChecksumValue = (String) tTableModel.getValueAt (aRow, tColumnIndex);
 		     		if (! tFirstChecksumValue.equals (tChecksumValue)) {
 		     			tIdenticalChecksums = false;
 		     		}
-		    		}
 	    		}
+    		}
        	}
    		if (tIdenticalChecksums) {
   			tCellRenderer.setBackground (Color.WHITE);
   		} else {
  			tCellRenderer.setBackground (Color.YELLOW);
   		}
+   		tCellRenderer.setForeground (Color.BLUE);
 	 
         return tCellRenderer;
     }
